be2136c1982d005692eb2614af95e9e96581c290
Arun Saradgi <saradgi07@gmail.com>
2023-02-24 23:36:43 +0530

    day-4

---
+++ fitfinancer/src/components/CorporateVideos.jsx
@@ -0,0 +1,63 @@
+import Navbar from "./navbar";
+
+export default function corporatevideos() {
+  return (
+    <div>
+      <Navbar />
+      <h1>CORPORATE VIDEOS</h1>
+      <div style={{ display: "flex", justifyContent: "center", gap: "25px" }}>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+      </div>
+      <br />
+      <div style={{ display: "flex", justifyContent: "center", gap: "25px" }}>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+      </div>
+      <br />
+      <div style={{ display: "flex", justifyContent: "center", gap: "25px" }}>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
